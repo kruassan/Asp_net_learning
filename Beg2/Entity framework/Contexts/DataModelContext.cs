477f61ee46f2a2caeacfa77902d0ade3c6397cf5
А <alexander.v_2010@mail.ru>
2018-07-26 13:03:01 +0300

    dev_2

---
+++ Beg2/Entity framework/Contexts/DataModelContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace Beg2.Entity_framework.Contexts
+{
+	public class DataModelContext : DbContext
+	{
+		public DataModelContext() : base("PostgreSql")
+		{
+
+		}
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder); // I had removed this
+												/// Rest of on model creating here.
+		}
+
+
+
+
+
+	}
+}
