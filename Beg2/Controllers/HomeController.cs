db131de9cea47383d637f588d15ae5e342223af4
А <alexander.v_2010@mail.ru>
2018-07-26 11:20:30 +0300

    dev_1

---
+++ Beg2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Beg2.Models;
+namespace Beg2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+			string dt = DateTime.Now.ToLongDateString();
+			ViewBag.datetime_now = dt;
+			return View();
+        }
+
+		[HttpGet]
+		public ViewResult Authentification()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult Authentification(AuthentificationData authData)
+		{
+			if (ModelState.IsValid)
+			{
+				return View("User", authData);
+			}
+			else
+			{
+				return View();
+			}
+		}
+	}
+}
---
+++ Beg2/Models/AuthentificationData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Beg2.Models
+{
+	public class AuthentificationData
+	{
+		[Required(ErrorMessage = "Please, enter your login")]
+		[RegularExpression(@"^\w+$", ErrorMessage = "Please, use correct login. Use only numbers and letters.")]
+		public string login { get; set; }
+
+
+		[Required(ErrorMessage = "Please, enter your password")]
+		public string password { get; set; }
+
+		[Required(ErrorMessage = "Please, select smth")]
+		public bool? remember { get; set; }
+	}
+}
